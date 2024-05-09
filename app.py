875d2438423c891ae6abeeb53b2f410ede45d114
Nirbhay2002 <110183517+Nirbhay2002@users.noreply.github.com>
2024-05-10 00:12:09 +0530

    Corrected encoding in training and server

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ CORS(app)
 
 # Load the trained model
 model = joblib.load('train_model.pkl')
+scaler = joblib.load('scaler.pkl')
 
 @app.route('/', methods=['POST'])
 
@@ -19,8 +20,11 @@ def predict():
     features = preprocess_data(data)
 
     # Make prediction
-    prediction = model.predict(features.reshape(1,-1))
+    print(features.reshape(1, -1))
+    prediction = model.predict(features.reshape(1, -1))
     prediction_list = prediction.tolist()
+    print(prediction_list)
+
 
     # Return prediction result
     return jsonify({'churn_prediction': prediction_list})
@@ -37,18 +41,19 @@ def preprocess_data(data):
 
     # Encode categorical variables
     
-    gender_mapping = {'Male': 0, 'Female': 1}  
-    subscription_mapping = {'Basic': 0, 'Standard': 1, 'Premium': 2}  
-    contract_mapping = {'Monthly': 0, 'Quarterly': 1, 'Annual': 2}
+    gender_mapping = {'Male': 1, 'Female': 0}  
+    subscription_mapping = {'Basic': 0, 'Standard': 2, 'Premium': 1}  
+    contract_mapping = {'Monthly': 1, 'Quarterly': 12, 'Annual': 0}
     gender = gender_mapping[data['gender']]
     subscription = subscription_mapping[data['subscription']]
     contract = contract_mapping[data['contract']]
 
-    processed_data = np.array([age,contract, delay, frequency, gender, interaction, spend, subscription, support, tenure])
+    processed_data = np.array([[age,gender, tenure, frequency, support, delay, subscription,contract, spend, interaction]])
 
-    print(processed_data)
+    scaled_data = scaler.transform(processed_data)
+    print(scaled_data)
 
-    return processed_data
+    return scaled_data
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
--- models/train_model.py
+++ models/train_model.py
@@ -6,20 +6,42 @@ dataset = pd.read_csv('customer_churn_dataset-testing-master.csv')
 x= dataset.iloc[:, 1: -1].values
 y= dataset.iloc[:, -1].values
 
+print(x[0:15, 1])
+# print(x[0:15, 7])
+# print(x[0:15, 6])
+
+
 from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
 x[:, 1] = le.fit_transform(x[:, 1])
 x[:, 7] = le.fit_transform(x[:, 7])
-
-
 x[:, 6] = le.fit_transform(x[:, 6])
 
 from sklearn.model_selection import train_test_split
 x_train , x_test, y_train , y_test = train_test_split(x, y , test_size=0.25 , random_state=0)
 
 
+from sklearn.preprocessing import RobustScaler
+sc= RobustScaler()
+x_train=sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+
+
 from sklearn.ensemble import RandomForestClassifier
-random_forest = RandomForestClassifier(n_estimators=500, random_state=42)
+random_forest = RandomForestClassifier(n_estimators=100, random_state=42)
 random_forest.fit(x_train, y_train)
 
+array = np.array([[ 22,0,  25,  14,   4,  27,   0,   0, 598,   9]])
+array = sc.transform(array)
+print(array)
+prediction = random_forest.predict(array)
+
+
+y_pred = random_forest.predict(x_test)
+
+from sklearn.metrics import accuracy_score
+
+# accuracy_random_forest = random_forest.score(y_pred, y_test)
+# print("Accuracy:", accuracy_random_forest)
 joblib.dump(random_forest, 'train_model.pkl')
+joblib.dump(sc, 'scaler.pkl')
